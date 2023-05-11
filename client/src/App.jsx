fc05b0d782803ef69543fa2378178ca2f3346d5b
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2023-05-11 18:54:24 +0530

    link registerpage to login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import{Route,Routes} from "react-router-dom";
 import IndexPage from './assets/pages/indexPage';
 import LoginPage from './assets/pages/LoginPage';
 import Layout from './Layout';
+import RegisterPage from './assets/pages/RegisterPage';
 function App() {
   return (
     <Routes>
       <Route path= "/" element={<Layout />}>
       <Route index element={<IndexPage />}/>
     <Route path="/login" element={<LoginPage />}/>
+    <Route path='/register' element={<RegisterPage />}/>
       </Route>
 </Routes> 
   
--- client/src/assets/pages/RegisterPage.jsx
+++ client/src/assets/pages/RegisterPage.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 
-export default function LoginPage(){
+export default function RegisterPage(){
     return (
         <div className="mt-4 grow flex  items-center justify-around">
-            <div className="mb-64">
-                <h1 className="text-4xl text-center mb-4">Login</h1>
+            <div className="mb-32">
+                <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className=" max-w-md mx-auto">
-            <input type="email" placeholder="Enter your name"/>
+            <input type="text" placeholder="Enter your name"/>
+            <input type="email" placeholder="your@email.com"/>
              <input type="password" placeholder="password"/>
              <button className="primary">Login</button>
             </form>
             <div className="text-center py-2 text-gray-500">
-                Don't have Account? <Link className="underline text-black" to={"/register"}>Register</Link>
+            Already have Account? <Link className="underline text-black" to={"/Login"}>Login</Link>
             </div>
-            
            </div>
 
         </div>
